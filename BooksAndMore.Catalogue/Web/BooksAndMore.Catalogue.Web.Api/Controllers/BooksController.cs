87027c9f4bae1186037e1af3579e72a58ca3d1a5
tiggris <tiggris@gmail.com>
2018-01-07 14:59:41 +0100

    Web application set up

--- BooksAndMore.Catalogue/Web/BooksAndMore.Catalogue.Web.Api/Controllers/BooksController.cs
+++ BooksAndMore.Catalogue/Web/BooksAndMore.Catalogue.Web.Api/Controllers/BooksController.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace BooksAndMore.Catalogue.Web.Api.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Route("api/books")]
+    public class BooksController : Controller
     {
         // GET api/values
         [HttpGet]
